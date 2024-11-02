5e6b7edc0c2cc4309fbbd8b63408724e3ab33ff1
Mikuの鬆 <palomiku@outlook.com>
2024-11-03 01:30:00 +0800

    Re： Remove Icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,11 @@
 import './global.css';
 import { Provider } from './components/provider';
-import { Oxanium, Noto_Serif_SC } from 'next/font/google';
 import type { ReactNode } from 'react';
-
-const oxanium = Oxanium({
-  subsets: ['latin'],
-  variable: '--font-oxanium',
-  display: 'swap'
-});
-
-const notoSerifSC = Noto_Serif_SC({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-  variable: '--font-noto-serif-sc',
-  display: 'swap'
-});
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="zh-CN" suppressHydrationWarning>
-      <body className={`${oxanium.variable} ${notoSerifSC.variable}`}>
+      <body>
         <Provider>
           {children}
         </Provider>
