61445b59dbcc0721aed8ed879bb5a4b726aaba3c
wuhang2003 <wuhang2003@users.noreply.github.com>
2025-02-06 10:27:26 +0000

    deploy: 7ff7c16173aa3d63d93a61b87ed41269a09723d2

--- _next/static/chunks/10.603be129637089f0.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[10],{4010:(t,e,a)=>{async function s(t,e,a,s){let r=new URLSearchParams;r.set("query",t),e&&r.set("locale",e),a&&r.set("tag",a);let c=await fetch(`${s.api??"/api/search"}?${r.toString()}`);if(!c.ok)throw Error(await c.text());return await c.json()}a.d(e,{fetchDocs:()=>s}),a(1658)}}]);
