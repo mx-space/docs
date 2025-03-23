d6ff6ac4e15c95f7d47d5f64321aec02d4f8ff8d
PaloMiku <PaloMiku@users.noreply.github.com>
2025-03-23 15:51:04 +0000

    deploy: fbdaf6120f26f7ec6776e8c5338249fac2bd370a

--- _next/static/chunks/app/api/search/route-2425aa2fdf106056.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[202],{4833:()=>{}},_=>{var e=e=>_(_.s=e);_.O(0,[191,35,358],()=>e(4833)),_N_E=_.O()}]);
---
+++ _next/static/chunks/app/api/search/route-92ee92958fb7f5ad.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[202],{8443:()=>{}},_=>{var e=e=>_(_.s=e);_.O(0,[937,350,358],()=>e(8443)),_N_E=_.O()}]);
