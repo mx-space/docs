bb099aa94f603da12f02fec7f2eef1f9cc0572f1
PaloMiku <PaloMiku@users.noreply.github.com>
2025-06-09 16:22:19 +0000

    deploy: 82b84c87c110d3cdc2728df8fb4bcd7294f282d2

---
+++ _next/static/chunks/pages/_app-cd412789a7d194d4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{8549:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(7478)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(8549),n(6112))),_N_E=_.O()}]);
